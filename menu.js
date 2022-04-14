223686d4b808795bbe1aa27f5ab70e6e352467ad
Willy <timefor@gmail.com>
2022-04-14 15:32:01 +0200

    Fix scroll lock and menu links

--- menu.js
+++ menu.js
@@ -1,18 +1,33 @@
-const mobileMenu =document.getElementById('mobile-menu');
+const mobileMenu = document.getElementById('mobile-menu');
 const btn = document.getElementById('menu-btn');
-const myLogo =document.getElementById('logo')
-function openMenu(){
-  btn.style.display = "none";
-  mobileMenu.style.display ="flex";
-  myLogo.style.display="none";
+const myLogo = document.getElementById('logo');
+
+const menu1 = document.getElementById('menu1');
+const menu2 = document.getElementById('menu2');
+const menu3 = document.getElementById('menu3');
+
+
+function preventScroll(e){
+    e.tpreventDefault();
+    e.stopPropagaion();
+    return false;
 }
 
-btn.addEventListener('click',openMenu)
-function closeMenu(){
+function openMenu() {
+    btn.style.display = "none";
+    mobileMenu.style.display ="flex";
+    myLogo.style.display="none";
+    document.querySelector('.scrllStop').addEventListener('wheel', preventScroll, {passive: false});
+}
+
+btn.addEventListener('click', openMenu)
+function closeMenu() {
     mobileMenu.style.display ="none";
     btn.style.display= "block";
     myLogo.style.display ="block";
+    document.querySelector('.scrollable').removeEventListener('wheel', preventScroll, {passive: false});  
 }
+
 document.addEventListener('click', (event) => {
     if(event.target.id === "close-menu") {
         closeMenu();
@@ -21,3 +36,8 @@ document.addEventListener('click', (event) => {
     }
 
 });
+
+
+menu1.addEventListener('click', closeMenu);
+menu2.addEventListener('click', closeMenu);
+menu3.addEventListener('click', closeMenu);
