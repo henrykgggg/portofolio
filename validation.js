5700cf7f942697cedeac6cf23eef9fa210ffe0d9
henry <88551100+henrykgggg@users.noreply.github.com>
2022-04-16 15:18:00 -0500

    solving linter issues

--- validation.js
+++ validation.js
@@ -4,11 +4,11 @@ const Errormessage = document.getElementById('form-error');
 Errormessage.textContent = 'Message not sent! Please write using only the small letters. Thank you.';
 
 function validationForm(e) {
-if (Email.value !== Email.value.toLowerCase()) {
+  if (Email.value !== Email.value.toLowerCase()) {
     Errormessage.style.display = 'block';
     e.preventDefault();
-} else {
-Errormessage.style.display = 'none';
-}
+  } else {
+    Errormessage.style.display = 'none';
+  }
 }
 formbutton.addEventListener('click', validationForm);
